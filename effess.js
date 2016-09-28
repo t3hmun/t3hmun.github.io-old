a5f14abff962a4fd8fa9dbe9b12b36f544bc0efb
mun <t3hmun@gmail.com>
2016-09-28 20:40:00 +0100

    Completed read all files in dir function.

--- effess.js
+++ effess.js
@@ -1,9 +1,10 @@
 /*
-Lib for doing file system stuff on a slightly higher level than fs.
+ Lib for doing file system stuff on a slightly higher level than fs.
  */
 'use strict';
 
 exports.ensureDirCreated = ensureDirCreated;
+exports.readFilesInDir = readFilesInDir;
 
 const fs = require('fs');
 const path = require('path');
@@ -35,6 +36,58 @@ function ensureDirCreated(dirPath) {
             } else {
                 resolve();
             }
+        });
+    });
+}
+
+/**
+ * Reads all the files in specified dir that match the filter. Not recursive.
+ * @param {string} dirPath - Read files in this dir.
+ * @param {function(string):boolean} filterFunc - Returns true on file-names that should be included. Default always true.
+ * @param {string} encoding - Defaults to 'utf-8'.
+ * @returns {Promise} - An array of objects with string properties {name, path, dir, data}.
+ */
+function readFilesInDir(dirPath, filterFunc, encoding) {
+    filterFunc = filterFunc || (()=>true);
+    encoding = encoding || 'utf-8';
+    return new Promise((resolve, reject)=> {
+        fs.readdir(dirPath, encoding, (err, files)=> {
+            if (err) {
+                reject(err);
+                return;
+            }
+            let reads = [];
+            let failed = false;
+            // In theory this sets the computer readings all the files in parallel.
+            // Disks generally don't like being asked to do things in parallel.
+            // However I think I'll trust node and the OS to deal with that detail, life is short.
+            files.forEach((file)=> {
+                let filePath = path.join(dirPath, file);
+                reads.push(new Promise((resolve, reject)=> {
+                    // Don't try to read anymore files if one has failed.
+                    if (failed) return;
+                    // Skip files that don't match the filter.
+                    if (!filterFunc(file)) return;
+                    fs.readFile(filePath, encoding, (err, data)=> {
+                        if (err) {
+                            failed = true;
+                            reject(err);
+                            return;
+                        }
+                        resolve({
+                            name: file,
+                            path: filePath,
+                            dir: dirPath,
+                            data: data
+                        });
+                    });
+                }));
+            });
+            Promise.all(reads).then((loadedFiles)=> {
+                resolve(loadedFiles);
+            }).catch((err)=> {
+                reject(err);
+            });
         });
     });
 }
